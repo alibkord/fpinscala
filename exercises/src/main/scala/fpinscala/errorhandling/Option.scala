666b229c18f0ff48d4e83645ef4710568040d9f7
Ali Kord <akord@atlassian.com>
2014-11-30 22:30:11 +1100

    Option and variance

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,19 +1,36 @@
 package fpinscala.errorhandling
 
-
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+//  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+//    case None => None
+//    case Some(a) => f(a)
+//  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => {
+      if (f(a)) this
+      else None
+    })
 }
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
@@ -38,11 +55,22 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+
+  //s = Seq(a), m = mean(s) => variance = mean({math.pow(x - m, 2) | each x in s})
+  def variance(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+}
+
+object OptionRunner {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
 }
